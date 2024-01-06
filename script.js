4ed6110a9b1e1995e34d811979bd814c2f9eb906
Swayam Shrestha <swayams24@gmail.com>
2024-01-07 00:28:56 +1100

    Add playRound() fucntion

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ function getComputerChoice(){
     return choice;
 
 }
+
+// Plays a single round of the game
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    let result, playerCount = 0, computerCount = 0;
+    if (playerSelection !== computerSelection){
+        switch (playerSelection + " " + computerSelection){
+            case "rock scissors":
+                result = `${playerSelection} wins against ${computerSelection}`;
+                playerCount++;
+                break;
+            case "rock paper":
+                result = `${playerSelection} loses against ${computerSelection}`;
+                computerCount++;
+                break;
+            case "paper rock":
+                result = `${playerSelection} wins against ${computerSelection}`;
+                playerCount++;
+                break;
+            case "paper scissors":
+                result = `${playerSelection} loses against ${computerSelection}`;
+                computerCount++;
+                break;
+            case "scissors rock":
+                result = `${playerSelection} loses against ${computerSelection}`;
+                computerCount++;
+                break;
+            case "scissors paper":
+                result = `${playerSelection} wins against ${computerSelection}`;
+                playerCount++;
+                break;
+        }
+    } else {
+        console.log("You tied.");
+        playerSelection = prompt();
+        computerSelection = getComputerChoice();
+        return playRound(playerSelection,computerSelection);
+    }
+
+    return [result, playerCount, computerCount];
+}
